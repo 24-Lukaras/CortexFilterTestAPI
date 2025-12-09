613e3d5a915ffe14594479171154cea35d639c3f
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2025-12-09 10:07:12 +0100

    feat: added value response helper

---
+++ Other/ValuesResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CortexFilterTestAPI.Other
+{
+    public class ValuesResponse
+    {
+        public const string JSON_SCHEMA = """
+                {
+                    "type": "object",
+                    "properties": {
+                        "values": {
+                            "type": "array",
+                            "items": {
+                                "type": "number"
+                            }
+                        }
+                    },
+                    "required": [ "values" ]
+                }
+                """;
+
+        [JsonPropertyName("values")]
+        public int[] Values { get; init; }
+    }
+}
